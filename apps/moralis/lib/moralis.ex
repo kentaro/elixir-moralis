7977a45da457d41722688bc1c45401916272d684
Kentaro Kuribayashi <kentarok@gmail.com>
2022-03-16 21:51:18 +0900

    add entrypoint module

--- apps/moralis/lib/moralis.ex
+++ apps/moralis/lib/moralis.ex
@@ -1,18 +1,22 @@
 defmodule Moralis do
   @moduledoc """
-  Documentation for `Moralis`.
+  Handle Tesla connections for Moralis.
   """
 
   @doc """
-  Hello world.
+  Configure an authless client connection
 
-  ## Examples
+  # Returns
 
-      iex> Moralis.hello()
-      :world
-
+  Tesla.Env.client
   """
-  def hello do
-    :world
+  @spec client() :: Tesla.Env.client
+  def client do
+    [
+      {Tesla.Middleware.BaseUrl, "https://deep-index.moralis.io/api/v2"},
+      {Tesla.Middleware.EncodeJson, engine: Poison},
+      {Tesla.Middleware.Headers, [{"x-api-key", System.get_env("MORALIS_API_KEY")}]}
+    ]
+    |> Tesla.client()
   end
 end
--- apps/moralis/mix.exs
+++ apps/moralis/mix.exs
@@ -28,6 +28,8 @@ defmodule Moralis.Core.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:tesla, "~> 1.2"},
+      {:poison, "~> 3.0"}
     ]
   end
 end
